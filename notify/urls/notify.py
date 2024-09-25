5b2b971c356a12e551549bc062889783e6fbe5c4
nanato12 <admin@okj.info>
2024-09-26 04:16:44 +0900

    update: NotifyURL

--- notify/urls/notify.py
+++ notify/urls/notify.py
@@ -3,6 +3,7 @@ from urllib.parse import urljoin
 
 class NotifyURL:
     HOST = "https://notify-bot.line.me"
-    WEP_LOGIN = urljoin(HOST, "login")
-
+    WEB_LOGIN = urljoin(HOST, "login")
+    WEB_MYPAGE = urljoin(HOST, "my")
+    ISSUE_TOKEN = urljoin(HOST, "my/personalAccessToken")
     API_GROUP_LIST = urljoin(HOST, "api/groupList")
