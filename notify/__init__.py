ec31dc190aff64bb996b5e545e986fad70f9e9cb
nanato12 <admin@okj.info>
2024-09-26 10:41:02 +0900

    update: Notify

--- notify/__init__.py
+++ notify/__init__.py
@@ -6,7 +6,7 @@ from typing import Any, Optional
 from urllib.parse import parse_qs, urljoin, urlparse
 
 from pydantic import BaseModel, Field, ValidationError
-from requests import JSONDecodeError, Session
+from requests import HTTPError, JSONDecodeError, Session
 
 from notify import config
 from notify.decorator import save_cookie
@@ -40,126 +40,179 @@ class Notify(BaseModel):
 
     email: str = Field(default="")
     password: str = Field(default="")
-    cookie_name: str = Field(default="notify")
+    session_name: str = Field(default="notify")
     session: Session = Field(init=False, default_factory=Session)
+
+    @property
+    def session_dir(self) -> str:
+        return path_join(config.COOKIE_DIR, self.session_name)
+
+    @property
+    def cookie_path(self) -> str:
+        return path_join(self.session_dir, "cookie.json")
+
+    @property
+    def cert_path(self) -> str:
+        return path_join(self.session_dir, "cert.json")
 
     @save_cookie
     def model_post_init(self, __context: Any) -> None:
+        makedirs(self.session_dir, exist_ok=True)
+
         if exists(self.cookie_path):
             # login with cookie
             with open(self.cookie_path) as j:
                 c = json.load(j)
             self.session.cookies.update(c)
-        elif all((self.email, self.password)):
-            # login with Email and Password
-            raise NotImplementedError("Email login not yet implemented.")
+            try:
+                self.get_group_list()
+                return
+            except GetGroupListException:
+                pass
+            except Exception as e:
+                raise e
+
+        self.login()
+
+    @save_cookie
+    def login(self) -> None:
+        if all((self.email, self.password)):
+            self.login_with_email()
         else:
-            # login with QR
-            r = self.session.get(NotifyURL.WEB_LOGIN)
-            params = parse_qs(urlparse(r.url).query)
-            csrf_token = extract_csrf(r)
+            self.login_with_qr()
 
-            logger.debug(f"{csrf_token=}")
-            logger.debug(f"{params=}")
+    def login_with_email(self) -> None:
+        raise NotImplementedError("Email login not yet implemented.")
 
-            qr_login_params = params.copy()
-            qr_login_params.pop("loginState")
-            qr_login_params["channelId"] = qr_login_params.pop(
-                "loginChannelId"
+    def login_with_qr(self) -> None:
+        # login with QR
+        self.session.cookies.clear()
+        r = self.session.get(NotifyURL.WEB_LOGIN)
+        params = parse_qs(urlparse(r.url).query)
+        csrf_token = extract_csrf(r)
+
+        logger.debug(f"{csrf_token=}")
+        logger.debug(f"{params=}")
+
+        qr_login_params = params.copy()
+        qr_login_params.pop("loginState")
+        qr_login_params["channelId"] = qr_login_params.pop("loginChannelId")
+
+        # get QR image url path
+        try:
+            res = QRLoginSessionResponse.model_validate(
+                session_j := (
+                    session_r := self.session.get(
+                        LINEAccessURL.QR_LOGIN_SESSION,
+                        params=qr_login_params,
+                    )
+                ).json()
             )
+            logger.debug(f"{res=}")
+        except ValidationError:
+            raise QRLoginSessionException(
+                f"Invalid response json: {session_j}"
+            )
+        except JSONDecodeError:
+            raise QRLoginSessionException(
+                f"Invalid response: [{session_r.status_code}] "
+                f"{session_r.url}"
+            )
+        except Exception as e:
+            raise e
 
-            # get QR image url path
-            try:
-                res = QRLoginSessionResponse.model_validate(
-                    session_j := (
-                        session_r := self.session.get(
-                            LINEAccessURL.QR_LOGIN_SESSION,
-                            params=qr_login_params,
-                        )
-                    ).json()
-                )
-                logger.debug(f"{res=}")
-            except ValidationError:
-                raise QRLoginSessionException(
-                    f"Invalid response json: {session_j}"
-                )
-            except JSONDecodeError:
-                raise QRLoginSessionException(
-                    f"Invalid response: [{session_r.status_code}] "
-                    f"{session_r.url}"
-                )
+        # get QR image
+        r = self.session.get(urljoin(LINEAccessURL.HOST, res.qrCodePath))
+        qr_path = path_join(config.TMP_DIR, f"{res.code}.png")
 
-            # get QR image
-            r = self.session.get(urljoin(LINEAccessURL.HOST, res.qrCodePath))
-            qr_path = path_join(config.TMP_DIR, f"{res.code}.png")
+        makedirs(dirname(qr_path), exist_ok=True)
+        with open(qr_path, "wb") as f:
+            f.write(r.content)
 
-            makedirs(dirname(qr_path), exist_ok=True)
-            with open(qr_path, "wb") as f:
-                f.write(r.content)
+        # wait QR login
+        print(f"Please Login with QR.\nQR code: {qr_path}")
+        countdown = Countdown(seconds=180, message="QR limit")
+        countdown.start()
 
-            # wait QR login
-            print(f"Please Login with QR.\nQR code: {qr_path}")
-            countdown = Countdown(seconds=180, message="QR limit")
-            countdown.start()
+        if exists(self.cert_path):
+            with open(self.cert_path) as j:
+                c = json.load(j)
+            self.session.cookies.update(c)
 
-            try:
-                res = QRLoginWaitResponse.model_validate(
-                    wait_j := (
-                        wait_r := self.session.get(LINEAccessURL.QR_LOGIN_WAIT)
-                    ).json()
-                )
-                countdown.stop()
-                logger.debug(f"{res=}")
-            except ValidationError:
-                raise QRLoginWaitException(f"Invalid response json: {wait_j}")
-            except JSONDecodeError:
-                raise QRLoginWaitException(
-                    f"Invalid response: [{wait_r.status_code}] {wait_r.url}"
-                )
+        try:
+            res = QRLoginWaitResponse.model_validate(
+                wait_j := (
+                    wait_r := self.session.get(LINEAccessURL.QR_LOGIN_WAIT)
+                ).json()
+            )
+            countdown.stop()
+            logger.debug(f"{res=}")
+        except ValidationError:
+            raise QRLoginWaitException(f"Invalid response json: {wait_j}")
+        except JSONDecodeError:
+            raise QRLoginWaitException(
+                f"Invalid response: [{wait_r.status_code}] {wait_r.url}"
+            )
+        except Exception as e:
+            raise e
 
-            if not res.pinCode:
-                raise AuthorizeException(f"[{res.errorCode}] {res.error}")
+        if res.errorCode:
+            raise AuthorizeException(f"[{res.errorCode}] {res.error}")
 
-            # wait input PIN code
-            print(f"Please input PIN code: {res.pinCode}")
-            countdown = Countdown(seconds=180, message="PIN limit")
-            countdown.start()
-
-            try:
-                res = QRLoginPINWaitResponse.model_validate(
-                    pin_j := (
-                        pin_r := self.session.get(
-                            LINEAccessURL.QR_LOGIN_PIN_WAIT
-                        )
-                    ).json()
-                )
-                countdown.stop()
-                logger.debug(f"{res=}")
-            except ValidationError:
-                raise QRLoginPINWaitException(
-                    f"Invalid response json: {pin_j}"
-                )
-            except JSONDecodeError:
-                raise QRLoginPINWaitException(
-                    f"Invalid response: [{pin_r.status_code}] {pin_r.url}"
-                )
-
+        if not res.redirectPath:
+            # PIN needed.
+            res = self.__wait_pin_for_qr_login(res)
             if not res.redirectPath:
                 raise AuthorizeException(f"[{res.errorCode}] {res.error}")
 
-            # login
-            login_params = params.copy()
-            login_params.update({"__csrf": csrf_token})  # type: ignore [dict-item]
-            r = self.session.get(
-                urljoin(LINEAccessURL.HOST, res.redirectPath),
-                params=login_params,
+        # login
+        login_params = params.copy()
+        login_params.update({"__csrf": csrf_token})  # type: ignore [dict-item]
+        r = self.session.get(
+            urljoin(LINEAccessURL.HOST, res.redirectPath),
+            params=login_params,
+        )
+        try:
+            r.raise_for_status()
+        except HTTPError as e:
+            raise AuthorizeException(e)
+        except Exception as e:
+            raise e
+
+    def __wait_pin_for_qr_login(
+        self, r: QRLoginWaitResponse
+    ) -> QRLoginPINWaitResponse:
+        print(f"Please input PIN code: {r.pinCode}")
+        countdown = Countdown(seconds=180, message="PIN limit")
+        countdown.start()
+
+        try:
+            res = QRLoginPINWaitResponse.model_validate(
+                pin_j := (
+                    pin_r := self.session.get(LINEAccessURL.QR_LOGIN_PIN_WAIT)
+                ).json()
+            )
+            countdown.stop()
+            logger.debug(f"{res=}")
+        except ValidationError:
+            raise QRLoginPINWaitException(f"Invalid response json: {pin_j}")
+        except JSONDecodeError:
+            raise QRLoginPINWaitException(
+                f"Invalid response: [{pin_r.status_code}] {pin_r.url}"
             )
 
-            logger.info(f"saved cookie: {self.cookie_path}")
+        with open(self.cert_path, "w") as f:
+            json.dump(
+                {
+                    k: v
+                    for k, v in self.session.cookies.get_dict().items()
+                    if k in ("cert", "qrPinCert")
+                },
+                f,
+                indent=2,
+            )
 
-    @property
-    def cookie_path(self) -> str:
-        return path_join(config.COOKIE_DIR, f"{self.cookie_name}.json")
+        return res  # type: ignore [no-any-return]
 
     def get_group_list(self) -> list[Group]:
         groups: list[Group] = []
@@ -180,6 +233,8 @@ class Notify(BaseModel):
                 raise GetGroupListException(
                     f"Invalid response: [{r.status_code}] {r.url}"
                 )
+            except Exception as e:
+                raise e
 
             if not res.results:
                 break
@@ -213,5 +268,7 @@ class Notify(BaseModel):
             raise IssueTokenException(
                 f"Invalid response: [{r.status_code}] {r.url}"
             )
+        except Exception as e:
+            raise e
 
         return res.token  # type: ignore [no-any-return]
