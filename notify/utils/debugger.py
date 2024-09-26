57d6e3530e9e85511262c830c195f211893607dc
nanato12 <admin@okj.info>
2024-09-26 10:43:01 +0900

    rename: debug

--- notify/utils/debugger.py
+++
@@ -1,8 +0,0 @@
-from requests import Response
-
-
-class Debugger:
-    @staticmethod
-    def save_html(r: Response, path: str = "index.html") -> None:
-        with open(path, "wt") as f:
-            f.write(r.text)
